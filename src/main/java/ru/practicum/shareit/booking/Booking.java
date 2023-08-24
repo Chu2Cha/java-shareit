5ab412b10b11384fbe97253e91fff55bc8ef4143
Boris Chuguev <chuguevb@gmail.com>
2023-08-24 18:00:15 +0300

    add-bookings - ver 0.3 (item - solve)

--- src/main/java/ru/practicum/shareit/booking/Booking.java
+++ src/main/java/ru/practicum/shareit/booking/Booking.java
@@ -1,27 +1,47 @@
-//package ru.practicum.shareit.booking;
-//
-//import lombok.Data;
-//import ru.practicum.shareit.item.model.Item;
-//import ru.practicum.shareit.user.model.User;
-//
-//import java.util.Date;
-//
-///**
-// * TODO Sprint add-bookings.
-// */
-//@Data
-//public class Booking {
-//    private int id;
-//    private Date start;
-//    private Date end;
-//    private Item item;
-//    private User booker;
-//    private Status status;
-//
-//    private enum Status {
-//        WAITING,
-//        APPROVED,
-//        REJECTED,
-//        CANCELED
-//    }
-//}
+package ru.practicum.shareit.booking;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ru.practicum.shareit.item.model.Item;
+import ru.practicum.shareit.user.model.User;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "bookings")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Booking {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "start_date", nullable = false)
+    private LocalDateTime start;
+
+    @Column(name = "end_date", nullable = false)
+    private LocalDateTime end;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "item_id", nullable = false)
+    private Item item;
+
+    @ManyToOne
+    @JoinColumn(name = "booker_id")
+    private User booker;
+
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    private enum Status {
+        WAITING,
+        APPROVED,
+        REJECTED,
+        CANCELED
+    }
+}
