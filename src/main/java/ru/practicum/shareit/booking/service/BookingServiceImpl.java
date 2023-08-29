f970c331ba887d968772e0f61a11dfa0d5c9c095
Chu2Cha <chuguevb@gmail.com>
2023-08-29 23:01:41 +0300

    ver 0.41

--- src/main/java/ru/practicum/shareit/booking/service/BookingServiceImpl.java
+++ src/main/java/ru/practicum/shareit/booking/service/BookingServiceImpl.java
@@ -3,15 +3,28 @@ package ru.practicum.shareit.booking.service;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.practicum.shareit.booking.dto.BookingDto;
+import ru.practicum.shareit.booking.dto.BookingMapper;
+import ru.practicum.shareit.booking.model.Booking;
+import ru.practicum.shareit.booking.repository.BookingRepository;
+import ru.practicum.shareit.user.service.UserService;
 
 import java.util.List;
 
 @Service
 @AllArgsConstructor
 public class BookingServiceImpl implements BookingService{
+
+    private final UserService userService;
+
+    private final BookingRepository bookingRepository;
+    private final BookingMapper bookingMapper;
+
     @Override
     public BookingDto createBooking(BookingDto bookingDto, long bookerId) {
-        return null;
+        userService.findUserById(bookerId);
+        Booking booking =  bookingMapper.toBooking(bookingDto);
+        bookingRepository.save(booking);
+        return bookingDto;
     }
 
     @Override
