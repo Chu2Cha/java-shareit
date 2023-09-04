a580c882bcc91853dcb4eba86eab1902837eb2bc
Boris Chuguev <chuguevb@gmail.com>
2023-09-04 17:48:03 +0300

    add-item-requests - version 0.1 (start)

--- src/main/java/ru/practicum/shareit/request/dto/ItemRequestDto.java
+++ src/main/java/ru/practicum/shareit/request/dto/ItemRequestDto.java
@@ -7,9 +7,7 @@ import ru.practicum.shareit.user.model.User;
 import javax.validation.constraints.NotBlank;
 import java.time.LocalDate;
 
-/**
- * TODO Sprint add-item-requests.
- */
+
 @Data
 @AllArgsConstructor
 public class ItemRequestDto {
---
+++ src/test/java/ru/practicum/shareit/user/UserControllerTest.java
@@ -0,0 +1,85 @@
+package ru.practicum.shareit.user;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import ru.practicum.shareit.user.dto.UserDto;
+import ru.practicum.shareit.user.service.UserService;
+
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+import static org.hamcrest.Matchers.is;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+@WebMvcTest(controllers = UserController.class)
+public class UserControllerTest {
+
+    @Autowired
+    ObjectMapper mapper;
+
+    @MockBean
+    private UserService userService;
+
+    @Autowired
+    private MockMvc mvc;
+
+    private UserDto userDto = new UserDto(1L, "Boris", "chuguevb@gamil.com");
+
+    @Test
+    void saveNewUser() throws Exception {
+        when(userService.saveUser(any()))
+                .thenReturn(userDto);
+
+        mvc.perform(post("/users")
+                        .content(mapper.writeValueAsString(userDto))
+                        .characterEncoding(StandardCharsets.UTF_8)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", is(userDto.getId()), Long.class))
+                .andExpect(jsonPath("$.name", is(userDto.getName())))
+                .andExpect(jsonPath("$.email", is(userDto.getEmail())));
+    }
+    @Test
+    void saveNewUserWithException() throws Exception {
+        when((userService.saveUser(any())))
+                .thenThrow(IllegalArgumentException.class);
+        mvc.perform(post("/users")
+                        .content(mapper.writeValueAsString(userDto))
+                        .characterEncoding(StandardCharsets.UTF_8)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(status().is(500));
+    }
+
+    @Test
+    void getAllUsers() throws Exception {
+        when(userService.getAllUsers())
+                .thenReturn(List.of(userDto));
+        mvc.perform(get("/users")
+                        .content(mapper.writeValueAsString(List.of(userDto)))
+                        .characterEncoding(StandardCharsets.UTF_8)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andDo(MockMvcResultHandlers.print())
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+    }
+
+}
+
